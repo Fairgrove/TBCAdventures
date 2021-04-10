3fd4d3db5a1da504a28505da74b46e3779476a7e
Fairgrove <fagerlundfr@gmail.com>
2021-04-10 05:34:24 +0200

    more data structure work

--- AdventureDB.lua
+++ AdventureDB.lua
@@ -1,57 +1,75 @@
 --NAMESPACE
 -------------------------------------
 local _, core = ...;
-core.nodeDB = {}; -- adds nodeData table to addon namespace
+core.nodeDB = {}; -- adds nodeDB table to addon namespace
 
 local nodeDB = core.nodeDB; -- so i dont have to type core.nodeData all the time
 
 --Actual Code
 -------------------------------------
 
-nodeDB.keys = {
-    ['name'] = 1,
-    ['x'] = 2,
-    ['y'] = 3,
-    ['textures'] = {
-        ['icon'] = {
-            ['texture'] = "dadadad",
-            ['color'] = {},
-        },
-        ['ring'] = {
-            ['texture'] = "",
-            ['color'] = {},
-        },
-        ['highlight'] = {
-            ['texture'] = "",
-            ['color'] = {},
-        },
-    }
-}
-
-nodeDB.nodeData = {
-    [1] = {
-        "debug Node", 
-        math.random(-300, 300), 
-        math.random(-300, 300), 
-        {
-            {
-                "interface/icons/inv_mushroom_10",
-                {1, 1, 1, 1},
-            },{
-                "Interface/Artifacts/Artifacts-PerkRing-Final-Mask",
-                {1, 1, 1, 1},
-            },{
-                "interface/icons/inv_mushroom_12",
-                {1, 1, 1, 1},
-            },
-        },
-    },
-    [2] = 1
-}
-
-function nodeDB:GetName()
-	return self.nodeData[1][1]
+local isCompleted = function(status)
+    if status then
+        return "true"
+    else
+        return "false"
+    end
 end
---core.nodeDB.nodeData[1][1]
---core.nodeDB.nodeData[nodeID]
+
+local isFriendly = function(id)
+    local _,_,status = GetFactionInfoByID(id)
+    if status >= 5 then
+        return "true"
+    else
+        return "false"
+    end
+end
+
+--DB
+-------------------------------------
+nodeDB.nodeData = {
+    [0] = {
+        ['name'] = "debug Node", 
+        ['x'] = -450, --math.random(-300, 300)
+        ['y'] = 330, 
+        ['type'] = 1, -- 1 = quest, 2 = easy, 3 = raid
+        ['unlocked'] = true, --ispurchased()
+        ['textures'] = {
+            ['icon']  = "interface/icons/inv_mushroom_10",
+            ['ring'] = "Interface/Artifacts/Artifacts-PerkRing-Final-Mask",
+            ['glow'] = "interface/icons/inv_mushroom_12",  
+            ['unpurchased'] = "interface/icons/inv_mushroom_11",
+        },
+        ['tooltip'] = {
+            ['titleText'] = "Obtain a Hearthstone",
+            ['objectives'] = {
+                ['item'] = {
+                    {"Hearthstone", 1}, -- item, items needed
+                    {"Hearthstone", 2}
+                },
+                ['quest'] = {
+                    28714, -- fel moss quest
+                    28713, -- balance of nature 
+                },
+                ['reputation'] = {
+                    {69, 5}, --isFriendly(69) -- Darnassus
+                    {1134, 5}
+                },
+                ['bosskill'] = {
+                    "yes",
+                    "yes",
+                },
+
+            },
+        }
+        
+    },
+}
+--[''] = 0,
+nodeDB.nodeIDs = {
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
+}
+
+
+
 --39 bubbles
--- core.lua
+++ core.lua
@@ -1,7 +1,7 @@
 --NAMESPACE
 -------------------------------------
 local _, core = ...; -- Namespace
-
+local nodeDB = core.nodeDB.nodeData
 
 --Debug Stuff
 -------------------------------------
@@ -10,28 +10,117 @@ debug = true
 --Actual Code
 -------------------------------------
 
-local function CreateSubFrame(self, x, y)
+
+--Tooltip Creation
+-------------------------------------
+--these will be functions in the future, give it a sting and it makes it that color
+local completedColor = "|cff90EE90" 
+local whiteColor = "|cffffffff"
+
+local function genItemText(items)
+    local str ="Items Needed:\n"
+            
+    for i in pairs(items) do
+        local itemName = items[i][1]
+        local itemCount = GetItemCount(items[i][1])
+        local itemReq = items[i][2]
+        if itemCount >= itemReq then
+            if itemReq == 1 then
+                str = str .. "|cffffffff - |r|cff90EE90" .. itemName .. "|r\n"
+            else
+                str = str .. "|cffffffff - |r|cff90EE90" .. itemName .. " " .. itemCount .. "/" .. itemReq .. "|r\n"
+            end
+        else
+            if itemReq == 1 then
+                str = str .. "|cffffffff - " .. itemName .. "|r\n"
+            else
+                str = str .. "|cffffffff - " .. itemName .. " " .. itemCount .. "/" .. itemReq .. "|r\n"
+            end
+        end
+    end
+    return str
+end
+
+local function genQuestText(questIDs)
+    local str ="Quest Requirements:\n"
+    
+    for i in pairs(questIDs) do
+        if C_QuestLog.IsQuestFlaggedCompleted(questIDs[i]) then
+            str = str .. "|cffffffff - |r|cff90EE90" .. C_QuestLog.GetTitleForQuestID(questIDs[i]) .. "|r\n"
+        else
+            str = str .. "|cffffffff - " .. C_QuestLog.GetTitleForQuestID(questIDs[i]) .. "|r\n"
+        end
+    end
+    return str
+end
+
+local function genRepText(reps)
+    --if hasRep or not isHeader then
+    local str ="Reputations Needed:\n"
+    
+    for i in pairs(reps) do
+        local name, _, standing= GetFactionInfoByID(reps[i][1])
+        local repReq = reps[i][2]
+        if standing >= repReq then
+            str = str .. "|cffffffff - |r|cff90EE90" .. getglobal("FACTION_STANDING_LABEL" .. repReq) .. " with " .. name .. "|r\n"
+        else
+            str = str .. "|cffffffff - " .. getglobal("FACTION_STANDING_LABEL" .. repReq) .. " with " .. name .. "|r\n"
+        end
+    end
+    return str
+end
+
+local function createTooltip(tooltipData)
+    local str = "|cffff00ff" .. tooltipData['titleText'] .."|r\n"
+
+    for key, objective in pairs(tooltipData['objectives']) do
+        if key == 'item' then
+            str = str .. genItemText(objective)
+        end
+        
+        if key == 'quest' then
+            str = str .. genQuestText(objective)
+        end
+        
+        if key == 'reputation' then
+            str = str .. genRepText(objective)
+        end
+        
+        -- if key == 'bosskill' then
+        --     str = str .. "Bosses Killed:\n"
+        -- end
+        
+        
+    end
+    
+    return str
+end
+
+local function CreateSubFrame(self, node)
     local f = CreateFrame("Button", "$parent_NODE"..#self.SubFrames+1, self)
     
     local iconDownscale = 19
     f:SetSize(100, 100)
-    
+
+    --icon
     f.Icon = f:CreateTexture(nil, "ARTWORK", nil, 1)
     --same as set SetAllPoints() this just resises the texture
     f.Icon:SetPoint("TOPLEFT", f ,"TOPLEFT", iconDownscale, -iconDownscale)  
     f.Icon:SetPoint("BOTTOMRIGHT", f ,"BOTTOMRIGHT", -iconDownscale, iconDownscale)
-    f.Icon:SetTexture("interface/icons/inv_mushroom_11")
+    f.Icon:SetTexture(node['textures']['icon'])
     f.Icon:SetVertexColor(1, 1, 1, 1)
 
+    --glow
     f.Glow = f:CreateTexture(nil, "ARTWORK", nil, 2)
     f.Glow:SetPoint("TOPLEFT", f ,"TOPLEFT", iconDownscale, -iconDownscale)
     f.Glow:SetPoint("BOTTOMRIGHT", f ,"BOTTOMRIGHT", -iconDownscale, iconDownscale)
-    f.Glow:SetTexture("interface/icons/inv_mushroom_11")
+    f.Glow:SetTexture(node['textures']['glow'])
     f.Glow:SetVertexColor(0, 0, 0, 0)
 
+    -- ring
     f.texture = f:CreateTexture(nil, "ARTWORK", nil, 3)
     f.texture:SetAllPoints()
-    f.texture:SetTexture("Interface/Artifacts/Artifacts-PerkRing-Final-Mask")
+    f.texture:SetTexture(node['textures']['ring'])
     f.texture:SetVertexColor(1, 1, 1, 1)
     
     --this could be used for ranks???? 
@@ -42,12 +131,12 @@ local function CreateSubFrame(self, x, y)
     -- f.text:SetPoint("CENTER")
     -- f.text:SetText(f:GetName())
 
-    f:SetPoint("CENTER", x, y)
+    f:SetPoint("CENTER", node['x'], node['y'])
     
     --On mouseover
     f:SetScript("OnEnter", function(self)
         GameTooltip:SetOwner(self, "ANCHOR_BOTTOMRIGHT", -25, 25);
-        GameTooltip:SetText(self:GetName())
+        GameTooltip:SetText(createTooltip(node['tooltip']))
         GameTooltip:Show()
         f.Glow:SetVertexColor(0.5, 0.5, 0.5, 1)
     end)
@@ -61,13 +150,12 @@ local function CreateSubFrame(self, x, y)
     --On Click
     f:SetScript("OnClick", function(self)
         if debug then print("Clicked: " .. f:GetName()) end
-        print(core.nodeDB:GetName()) -- name of node
     end)
     return f
 end
 
 local UI = CreateFrame("Frame", "Adventure_UI", UIParent, "BasicFrameTemplateWithInset")
-UI:SetSize(1000, 800)
+UI:SetSize(1000, 800) --x, y
 UI:SetPoint("CENTER", UIParent, "CENTER")
 UI.SubFrames = {}
 
@@ -76,8 +164,8 @@ UI.title:SetFontObject("GameFontHighlight")
 UI.title:SetPoint("TOP", 0, -5)
 UI.title:SetText("Adventure Guide")
 
-for i= 1, 5 do
-    tinsert(UI.SubFrames, CreateSubFrame(UI, math.random(-300, 300), math.random(-300, 300)))
+for i= 1, 1 do
+    tinsert(UI.SubFrames, CreateSubFrame(UI, nodeDB[0]))
 end
 
 -- for debugging
@@ -98,7 +186,7 @@ else
 end
 
 -- this has to be at the end of the code, idk why.
-tinsert(UISpecialFrames, UI:GetName())
-UI:SetScript("OnEscapePressed", function()
-    UI:Hide()
-end)
+-- tinsert(UISpecialFrames, UI:GetName())
+-- UI:SetScript("OnEscapePressed", function()
+--     UI:Hide()
+-- end)
